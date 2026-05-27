a3f5dc56904cd4b476db34139ccf4ce33ef9b07a
오예진 <103239736+oyj08098@users.noreply.github.com>
2026-05-27 19:19:33 +0900

    Create hw01_led.py

---
+++ hw01_led.py
@@ -0,0 +1,14 @@
+#hw01_led.py
+from gpiozero import LED
+from time import sleep
+
+led = LED(17)  # GPIO17, physical pin 11
+
+while True:
+    led.on()
+    print("LED ON")
+    sleep(1)
+
+    led.off()
+    print("LED OFF")
+    sleep(1)
